6b89b11065014773852efbeb21eb8eb83948c99d
Dorian-Cesar <dorian.celu@gmail.com>
2024-05-22 11:05:20 -0400

    cambio de url backend

--- php/api.php
+++ php/api.php
@@ -1,4 +1,13 @@
 <?php
+// Permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+
+// Permitir los métodos de solicitud que se utilizarán
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+// Permitir ciertos encabezados en las solicitudes preflight OPTIONS, incluido Content-Type
+header("Access-Control-Allow-Headers: Content-Type");
+
 $servername = "ls-8ce02ad0b7ea586d393e375c25caa3488acb80a5.cylsiewx0zgx.us-east-1.rds.amazonaws.com";
 $username = "dbmasteruser";
 $password = ':&T``E~r:r!$1c6d:m143lzzvGJ$NuP;';
