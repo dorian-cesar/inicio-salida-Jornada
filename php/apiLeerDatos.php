84e537d288a287a676f9ee1b30b659075e52b394
Dorian-Cesar <dorian.celu@gmail.com>
2024-06-04 14:53:39 -0400

    fix Cors

--- php/apiLeerDatos.php
+++ php/apiLeerDatos.php
@@ -1,4 +1,14 @@
 <?php
+
+// Permitir solicitudes desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+
+// Permitir los métodos de solicitud que se utilizarán
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+// Permitir ciertos encabezados en las solicitudes preflight OPTIONS, incluido Content-Type
+header("Access-Control-Allow-Headers: Content-Type");
+
 // Conexión a la base de datos
 
 $servername = "ls-8ce02ad0b7ea586d393e375c25caa3488acb80a5.cylsiewx0zgx.us-east-1.rds.amazonaws.com";
